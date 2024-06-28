c6fca028ae2c712171c190e0f37cbd9588104b63
Michał Woliński <s29239@pjwstk.edu.pl>
2024-06-28 13:35:49 +0200

    final project

--- ProjektAPBD/ApiTests/TestObjects/FakeUserRepository.cs
+++ ProjektAPBD/ApiTests/TestObjects/FakeUserRepository.cs
@@ -11,7 +11,36 @@ public class FakeUserRepository : IUserRepository
     {
         _users = new List<User>
         {
-
+            new User
+            {
+                UserId = 1,
+                Login = "admin",
+                Password = "CrnyJzGKqI2hdJjjj/fVvFF1DE1IxzuqFp8GIPwMSPQ=",
+                Salt = "3/VW5z1kkOo+uUFfALuMTQ==",
+                Role = "admin",
+                RefreshToken = "iLEtLuCF/L0lkIfnTQEBX8q2HLVui4GkUCX2TEVj0ls=",
+                RefreshTokenExpiration = DateTime.Now.AddDays(1)
+            },
+            new User
+            {
+                UserId = 2,
+                Login = "user",
+                Password = "NGdQGRBVlul5nqSLofzx3Rs9+nwtkf5h7xoFdsyYtP0=",
+                Salt = "5ph7adtYHPSVyTWuYDDmcQ==",
+                Role = "user",
+                RefreshToken = "+wyC5r2q5ySFDjOnTGG/P8Td/yutHA4aUKQ7mqktW+g=",
+                RefreshTokenExpiration = DateTime.Now.AddDays(1)
+            },
+            new User
+            {
+                UserId = 1,
+                Login = "admin",
+                Password = "CrnyJzGKqI2hdJjjj/fVvFF1DE1IxzuqFp8GIPwMSPQ=",
+                Salt = "3/VW5z1kkOo+uUFfALuMTQ==",
+                Role = "admin",
+                RefreshToken = "iLEtLuCF/L0lkIfnTQEBX8q2HLVui4GkUCX2TEVj0las=",
+                RefreshTokenExpiration = DateTime.Now.AddDays(-2)
+            }
         };
     }
 
---
+++ ProjektAPBD/ApiTests/UserServiceTests.cs
@@ -0,0 +1,95 @@
+﻿using Api.Exceptions;
+using Api.Interfaces;
+using Api.RequestModels;
+using Api.Services;
+using ApiTests.TestObjects;
+using Microsoft.Extensions.Configuration;
+using Shouldly;
+
+namespace ApiTests;
+
+public class UserServiceTests
+{
+    private readonly IUserService _userService;
+
+    public UserServiceTests()
+    {
+        _userService = new UserService(new FakeUserRepository(), new ConfigurationBuilder().Build());
+    }
+
+    [Fact]
+    public async Task Should_ThrowException_WhenLoginIsEmpty()
+    {
+        var command = new LoginRequestDto
+        {
+            Login = "",
+            Password = "user"
+        };
+
+        await Should.ThrowAsync<DomainException>
+            (_userService.Login(command, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task Should_ThrowException_WhenPasswordIsEmpty()
+    {
+        var command = new LoginRequestDto
+        {
+            Login = "user",
+            Password = ""
+        };
+
+        await Should.ThrowAsync<DomainException>
+            (_userService.Login(command, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task Should_ThrowException_WhenUserDoesNotExist()
+    {
+        var command = new LoginRequestDto
+        {
+            Login = "user1",
+            Password = "user"
+        };
+
+        await Should.ThrowAsync<DomainException>
+            (_userService.Login(command, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task Should_ThrowException_WhenPasswordsDoNotMatch()
+    {
+        var command = new LoginRequestDto
+        {
+            Login = "user",
+            Password = "user1"
+        };
+
+        await Should.ThrowAsync<DomainException>
+            (_userService.Login(command, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task Should_ThrowException_WhenUserDoesNotExistRefresh()
+    {
+        var command = new RefreshTokenDto
+        {
+            RefreshToken = "iLEtLuCF/L0lkIfnTQEBX8q2HLVui4GkUCX2TEVj0ls=a"
+        };
+
+        await Should.ThrowAsync<DomainException>
+            (_userService.Refresh(command, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task Should_ThrowException_WhenTokenIsInvalid()
+    {
+        var command = new RefreshTokenDto
+        {
+            RefreshToken = "iLEtLuCF/L0lkIfnTQEBX8q2HLVui4GkUCX2TEVj0las="
+        };
+
+        await Should.ThrowAsync<DomainException>
+            (_userService.Refresh(command, CancellationToken.None));
+    }
+}
